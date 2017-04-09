8bd05a886b0d914b2f4ef50cb11d602147c5fc44
kranthi veeramachaneni <kranthi1056@gmail.com>
2017-04-09 22:17:55 +0100

    added pouchdb for offline application

--- dist/content/contentScript.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var contentScript = (function () {
-    function contentScript() {
-    }
-    return contentScript;
-}());
-exports.default = contentScript;
-//# sourceMappingURL=contentScript.js.map
