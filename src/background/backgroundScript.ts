742fa1c9cbbb23b7cfd1c391e8d398154e21e08d
kranthi veeramachaneni <kranthi1056@gmail.com>
2017-04-09 21:46:47 +0100

    Code Cleanup

--- src/background/backgroundScript.ts
+++ src/background/backgroundScript.ts
@@ -5,16 +5,22 @@ export default class backgroundScript {
         console.log("menu started creating");
         this.menuItems.forEach(element => {
             let id = chrome.contextMenus.create(
-            element
-        );    
-        console.log(`manu id is ${id}`);
+                element
+            );
+            console.log(`manu id is ${id}`);
         });
     }
     private CreateMenus() {
         this.menuItems.push({
             'contexts': ["selection"],
-            'title': "Add selection to journey"
+            'title': "Add selection to journey",
+            onclick: this.onclick
         });
+    }
+    private onclick(info: any, tab: any) {
+        console.log("item " + info.menuItemId + " was clicked");
+        console.log("info: " + JSON.stringify(info));
+        console.log("tab: " + JSON.stringify(tab));
     }
 }
 
